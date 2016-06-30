960645717e7aa4e2c58c31ed44563a698041c0f1
lidor <lidor19592@gmail.com>
2016-06-30 13:32:18 +0300

    Ex8.2

--- MailSystem/MailSystem/Program.cs
+++ MailSystem/MailSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace MailSystem
 {
@@ -13,9 +14,8 @@ namespace MailSystem
             MailManager mailManager = new MailManager();
 
             mailManager.MailArrived += MailManager_MailArrived;
-
             mailManager.SimulateMailArrived(); //Create a System.Threading.Timer, and in the timer callback call SimulateMailArrived every 1 second
-
+            
         }
 
         public static void MailManager_MailArrived(object sender, MailArrivedEventArgs e)
