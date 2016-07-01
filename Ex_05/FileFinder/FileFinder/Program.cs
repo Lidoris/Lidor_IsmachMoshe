459fa295d9773e7634833ece29350f47cfddebbd
lidor <lidor19592@gmail.com>
2016-07-01 12:42:32 +0300

    10.2

--- Ex_05/FileFinder/FileFinder/Program.cs
+++ Ex_05/FileFinder/FileFinder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace FileFinder
     {
         static void Main(string[] args)
         {
+            string[] Files = Directory.GetFiles(args[0]);
+            string Substring = args[1];
+            List<string> filesWithSubstring;
+            
+            filesWithSubstring = FindFilesBySubstring(Files, Substring);
+
+            foreach (string path in filesWithSubstring)
+            {
+                Console.WriteLine(path);
+            }
+        }
+
+        static List<string> FindFilesBySubstring(string[] dirs, string Substring)
+        {
+            List<string> filesWithSubstring = new List<string>();
+
+            foreach (string path in dirs)
+            {
+                if (path.Contains(Substring))
+                {
+                    filesWithSubstring.Add(path);
+                }
+            }
+
+            return filesWithSubstring;
         }
     }
 }
