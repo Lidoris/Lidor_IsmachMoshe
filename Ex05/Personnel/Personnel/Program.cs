287f6b224740cf56d5b6c189b08be1db3cb3489a
lidor <lidor19592@gmail.com>
2016-07-01 11:45:26 +0300

    10.1

---
+++ Ex05/Personnel/Personnel/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listOfData;
+
+            try
+            {
+                listOfData = ReadData("mydata.txt");
+                foreach (string line in listOfData)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static List<string> ReadData(string path)
+        {
+            List<string> listOfData = new List<string>();
+            StreamReader reader = new StreamReader(path);
+            string line;
+
+            line = reader.ReadLine();
+            while (line != null)
+            {
+                listOfData.Add(line);
+                line = reader.ReadLine();
+            }
+
+            return listOfData;
+        }
+    }
+}
