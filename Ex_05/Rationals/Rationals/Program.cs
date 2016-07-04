55d1016df94379cfb7b08b90a2a4e917fd9b40e4
lidor <lidor19592@gmail.com>
2016-07-04 10:24:22 +0300

    10.3

---
+++ Ex_05/Rationals/Rationals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rationals;
+
+namespace Rationals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Rational rationalA = new Rational(4, 0);
+                Rational rationalB = new Rational(1, 3);
+
+                Console.WriteLine(" rationalA Value : {0}", rationalA);
+                Console.WriteLine(" rationalB Value : {0}", rationalB);
+
+                Console.WriteLine(" The sum of rationalA and rationalB is : {0}", rationalA + rationalB);
+                Console.WriteLine(" The result of multiplying rationalA and rationalB is : {0}", rationalA * rationalB);
+                Console.WriteLine(" The result after subtract rationalB from rationalA is : {0}", rationalA - rationalB);
+                Console.WriteLine(" The result after Divide rationalA by rationalB is : {0}", rationalA / rationalB);
+
+                rationalA.Reduce();
+                Console.WriteLine(" rationalA Value after reduce : {0}", rationalA);
+                Console.WriteLine(" Is rationalA and rationalB are Equals: {0}", rationalA.Equals(rationalB));
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Ex_05/Rationals/Rationals/Rational.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    public struct Rational
+    {
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+
+        public double Value
+        {
+            get
+            {
+                if(Denominator != 0)
+                {
+                    return Numerator / (double)Denominator;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public Rational(int numerator, int denominator)
+        {
+            if(denominator != 0)
+            {
+                Numerator = numerator;
+                Denominator = denominator;
+            }
+            else
+            {
+                throw new ArgumentException("The denominator can not be zero");
+            }
+            
+        }
+
+        public Rational(int numerator)
+        {
+            Numerator = numerator;
+            Denominator = 1;
+        }
+
+        public static Rational operator+(Rational numA, Rational numB)
+        {
+            Rational result;
+
+            if (numA.Denominator == numB.Denominator)
+            {
+                result = new Rational(numA.Numerator + numB.Numerator, numA.Denominator);
+            }
+            else
+            {
+                result = new Rational((numA.Numerator * numB.Denominator) + (numB.Numerator * numA.Denominator), numA.Denominator * numB.Denominator);
+            }
+           
+
+            return result;
+        }
+
+        public static Rational operator-(Rational numA, Rational numB)
+        {
+            Rational result;
+
+            if (numA.Denominator == numB.Denominator)
+            {
+                result = new Rational(numA.Numerator - numB.Numerator, numA.Denominator);
+            }
+            else if (numA.Value != numB.Value)
+            {
+                result = new Rational((numA.Numerator * numB.Denominator) - (numB.Numerator * numA.Denominator), numA.Denominator * numB.Denominator);
+            }
+            else
+            {
+                result = new Rational(0, 0);
+            }
+
+            return result;
+
+        }
+
+        public static Rational operator*(Rational numA, Rational numB)
+        {
+            Rational result;
+
+            result = new Rational(numA.Numerator*numB.Numerator, numA.Denominator*numB.Denominator);
+           
+            return result;
+        }
+
+        public static Rational operator /(Rational numA, Rational numB) 
+        {
+            Rational result;
+
+            result = new Rational(numA.Numerator * numB.Denominator, numA.Denominator * numB.Numerator);
+
+            return result;
+        }
+
+
+        public void Reduce ()
+        {
+            int min = Math.Min(Numerator, Denominator);
+            if (this.Numerator % this.Denominator == 0 || this.Denominator % this.Numerator == 0)
+            {
+                this.Numerator = this.Numerator / min;
+                this.Denominator = this.Denominator / min;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool result;
+
+            if ((this.Numerator == ((Rational)obj).Numerator) && (this.Denominator == ((Rational)obj).Denominator))
+            { 
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder rationalStr = new StringBuilder();
+
+            rationalStr.AppendFormat("[ {0}/{1} ] = {2:0.###}", Numerator, Denominator, Value);
+            return rationalStr.ToString();
+        }
+    }
+}
