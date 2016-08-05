00d803ea81da6dc82f516880dc37f98d8d40ffc5
lidor <lidor19592@gmail.com>
2016-08-05 12:02:21 +0300

    Tasks- lab 2

--- Advanced Labs/Primes/Primes/Program.cs
+++ Advanced Labs/Primes/Primes/Program.cs
@@ -13,11 +13,17 @@ namespace Primes
             List<long> listOfPrimes = new List<long>();
             try
             {
-                listOfPrimes = CalcPrimes(2, 10, 2);
-                foreach (var number in listOfPrimes)
-                {
-                    Console.WriteLine(number);
-                }
+                // Lab 1: Using the Parallel Class
+                //listOfPrimes = CalcPrimes(2, 30 , 4);
+                //foreach (var number in listOfPrimes)
+                //{
+                //    Console.WriteLine(number);
+                //}
+                
+                // Lab 2: Cancellation
+                listOfPrimes = CalcPrimes(2, 30000000);
+                Console.WriteLine("numbers returned before being cancelled {0}", listOfPrimes.Count);
+
             }
             catch (ArgumentOutOfRangeException e)
             {
@@ -25,11 +31,60 @@ namespace Primes
             }
         }
 
+        //Lab 2: Cancellation
+        private static List<long> CalcPrimes(long firstNum, long secondNum)
+        {
+            List<long> ListOfPrimes = new List<long>();
+            bool isPrime;
+            long minNum, maxNum;
+            Random rand = new Random();
+            object lockObj = new object();
+
+            minNum = Math.Min(firstNum, secondNum);
+            minNum = minNum < 1 ? 2 : minNum;
+            maxNum = Math.Max(firstNum, secondNum);
+
+            if (minNum < maxNum)
+            {
+                Parallel.For(minNum, maxNum, (i , x ) =>
+                {
+                    if (rand.Next(10000000) == 0)
+                    {
+                        x.Stop();
+                    }
+
+                    isPrime = true;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrime)
+                    {
+                        lock (lockObj)
+                        { 
+                            ListOfPrimes.Add(i);
+                        }
+                        
+                    }
+                });
+            }
+
+            return ListOfPrimes;
+        }
+
+       
+        //Lab 1: Using the Parallel Class
         private static List<long> CalcPrimes(long firstNum, long secondNum , int degree)
         {
             List<long> ListOfPrimes = new List<long>();
             bool isPrime;
             long minNum, maxNum;
+            object lockObj = new object();
 
             minNum = Math.Min(firstNum, secondNum);
             minNum = minNum < 1 ? 2 : minNum;
@@ -51,8 +106,11 @@ namespace Primes
 
                     if (isPrime)
                     {
-                        ListOfPrimes.Add(i);
-                    }
+                         lock (lockObj)
+                         {
+                             ListOfPrimes.Add(i);
+                         }
+                     }
                  });
             }
 
