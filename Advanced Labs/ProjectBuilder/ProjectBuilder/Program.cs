a067c1f47c1fde40a0fd13446bec0721f5639ffb
lidor <lidor19592@gmail.com>
2016-08-05 12:30:31 +0300

    Tasks- lab 3

--- Advanced Labs/ProjectBuilder/ProjectBuilder/Program.cs
+++ Advanced Labs/ProjectBuilder/ProjectBuilder/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProjectBuilder
@@ -10,6 +11,62 @@ namespace ProjectBuilder
     {
         static void Main(string[] args)
         {
+            var projectATask = new Task(() =>
+            {
+                Project projectA = new Project(1);
+                projectA.BuildProject();
+            });
+
+            var projectBTask = new Task(() =>
+            {
+                Project projectB = new Project(2);
+                projectB.BuildProject();
+            });
+
+            var projectCTask = new Task(() =>
+            {
+                Project projectC = new Project(3);
+                projectC.BuildProject();
+            });
+
+            var projectDTask = projectATask.ContinueWith(x =>
+            {
+                Project projectD = new Project(4);
+                projectD.BuildProject();
+            });
+
+            var projectETask = Task.Factory.ContinueWhenAll(new Task[] { projectATask, projectBTask, projectCTask } ,(x =>
+            {
+                Project projectE = new Project(5);
+                projectE.BuildProject();
+            }));
+
+            var projectFTask = Task.Factory.ContinueWhenAll(new Task[] { projectCTask, projectDTask }, (x =>
+            {
+                Project projectF = new Project(6);
+                projectF.BuildProject();
+            }));
+
+            var projectGTask = Task.Factory.ContinueWhenAll(new Task[] { projectETask, projectFTask }, (x =>
+            {
+                Project projectG = new Project(7);
+                projectG.BuildProject();
+            }));
+
+            var projectHTask = Task.Factory.ContinueWhenAll(new Task[] { projectETask}, (x =>
+            {
+                Project projectH = new Project(8);
+                projectH.BuildProject();
+            }));
+
+            projectATask.Start();
+            projectBTask.Start();
+            projectCTask.Start();
+            projectDTask.Wait();
+            projectETask.Wait();
+            projectFTask.Wait();
+            projectGTask.Wait();
+            projectHTask.Wait();
         }
     }
 }
--- Advanced Labs/ProjectBuilder/ProjectBuilder/Project.cs
+++ Advanced Labs/ProjectBuilder/ProjectBuilder/Project.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProjectBuilder
 {
     class Project
     {
+        public int ID { get; private set; }
+
+        public Project(int id)
+        {
+            ID = id;
+        }
+
+        public void BuildProject()
+        {
+            Console.WriteLine("Start building project number {0}...", ID);
+            Thread.Sleep(1000);
+            Console.WriteLine("Finished {0}", ID);
+        }
     }
 }
