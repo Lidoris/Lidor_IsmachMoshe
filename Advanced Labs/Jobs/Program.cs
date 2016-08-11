54f6208f70d58873bb6e9cd95bf88652b699f50a
lidor <lidor19592@gmail.com>
2016-08-11 14:18:06 +0300

    The Dispose Pattern - Part A

---
+++ Advanced Labs/Jobs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Jobs
+{
+	class Program
+    {
+		static void Main(string[] args)
+        {
+            using (Job job = new Job())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Process process = Process.Start("Notepad");
+                    job.AddProcessToJob(process);
+                }
+
+                Console.WriteLine("Press enter to kill all the processes");
+                Console.ReadLine();
+                job.Kill();
+            }
+        }
+	}
+}
